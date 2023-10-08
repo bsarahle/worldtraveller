ffd2605c931ca5334e579d8811fb0e97ea2ad4de
Sarah <elharas0088@gmail.com>
2023-10-07 20:05:13 -0400

    added the package json and edited pipeline

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: 'production',
+  entry:'./src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CopyPlugin({
+        patterns: [
+          { from: 'src/css', to: 'css' },
+        ],
+      }),
+  ]
+};
